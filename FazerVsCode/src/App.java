9ac4c458d4cfd2ac2158550c201c8cfc428d8739
UDVSK <142414238+UDVSK@users.noreply.github.com>
2024-09-11 10:29:40 -0300

    Update App.java
    Atualizado para se adequar as alterações feitas na classe "eu"

--- FazerVsCode/src/App.java
+++ FazerVsCode/src/App.java
@@ -1,29 +1,15 @@
-import java.util.Scanner;
-
 public class App {
-        //Comentário
+    public static void main(String[] args) {
+      eu Matheus = new eu();
+      Matheus.setCurso("Curso: " + "ADS");
+      Matheus.setIdlinkedin("Linked-in: " + "matheus-henrique-rodrigues-ferrari-838317195");
+      Matheus.setIdgit("GitHub: " + "UDVSK");
+      Matheus.setRa("RA: " + "2920482221018");
+      Matheus.setIdade("Idade: " + "21");
+      Matheus.setNome("Nome: " + "Matheus Henrique Rodrigues Ferrari");
 
-    public static void funcao_teste (){
-        //Biblioteca para entrada de dados
-        Scanner ler = new Scanner(System.in);
-
-        //variável local 
-        String v2;
-        int v1;
-
-        System.out.println("Comando de Saida de Dados");
-        
-        System.out.printf("Informe uma palavra: ");
-        v2 = ler.next();
-
-        System.out.printf("Informe um número: ");
-        v1 = ler.nextInt();
-
-        System.out.printf(v2+" "+v1);
-    }
-    public static void main(String[] args) throws Exception {
-        funcao_teste();
+      Matheus.explanar();
 
 
     }
-}
+   }
